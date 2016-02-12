44fa60b7c4291bc7bf4c16e6a850521c01074d7f
captus <utility.ship.captain@gmail.com>
2016-02-11 20:53:20 -0800

    PEP8 update for interactive CLI
    Used pylint to get card_cli up to PEP8 standards for Python 3

--- card_cli.py
+++ card_cli.py
@@ -5,16 +5,18 @@ An interactive shell interface to various functions
 
 import argparse
 import inspect
-import sys
 import traceback
 import types
 
+import card_functions
+import card_sets
+
 ################################################################################
 # Create a parser for loading options on the interactive CLi
-cli_parser = argparse.ArgumentParser()
-cli_parser.add_argument('-s', action="store_true", dest="short",
-        help="Shorten function names")
-cli_args = cli_parser.parse_args()
+CLI_PARSER = argparse.ArgumentParser()
+CLI_PARSER.add_argument('-s', action="store_true", dest="short",
+                        help="Shorten function names")
+CLI_ARGS = CLI_PARSER.parse_args()
 
 ################################################################################
 # Create a custom ArgumentParser class for the interactive CLI
@@ -24,9 +26,15 @@ class ArgumentParser(argparse.ArgumentParser):
     Local class to over-ride default ArgumentParser error function.
     """
     class Error(Exception):
+        """
+        A custom error class for catching only the exceptions we can handle.
+        """
         pass
 
     class TooFewArguments(Exception):
+        """
+        A custom error class for indicating some help docs are needed.
+        """
         pass
 
     def error(self, message):
@@ -41,41 +49,47 @@ class ArgumentParser(argparse.ArgumentParser):
 
 PARSERS = dict()
 
-parser = ArgumentParser(prog='', add_help=False)
-PARSERS['base'] = parser
-subparsers = parser.add_subparsers(title='actions')
+PARSER = ArgumentParser(prog='', add_help=False)
+PARSERS['base'] = PARSER
+SUBPARSERS = PARSER.add_subparsers(title='actions')
 
 ################################################################################
 
 # Check the card_functions module for any interface functions that
 # may be helpful for use in a CLI
-import card_functions
 CARD_FUNCTIONS = dict()
 for thing in dir(card_functions):
     if thing.endswith("_card"):
         card_func = getattr(card_functions, thing)
-        if type(card_func) == types.FunctionType:
+        if isinstance(card_func, types.FunctionType):
+            # pylint: disable=deprecated-method
+            # Complaining about getargspec being deprecated
             CARD_FUNCTIONS[thing] = {
-                    "function": card_func,
-                    "argspec": inspect.getargspec(card_func)
-                    }
+                "function": card_func,
+                "argspec": inspect.getargspec(card_func)
+                }
 
 # Now check the card_set module for functions
-import card_sets
 SET_FUNCTIONS = dict()
 for thing in dir(card_sets):
     set_func = getattr(card_sets, thing)
-    if type(set_func) == types.FunctionType:
+    if isinstance(set_func, types.FunctionType):
+        # pylint: disable=deprecated-method
+        # Complaining about getargspec being deprecated
         SET_FUNCTIONS[thing] = {
-                "function": set_func,
-                "argspec": inspect.getargspec(set_func)
-                }
+            "function": set_func,
+            "argspec": inspect.getargspec(set_func)
+            }
 
 ################################################################################
 
-def name_str(name):
-    if cli_args.short:
-        first, second = name.split('_', 1)
+def name_str(func_name):
+    """
+    Return an action name for the function. By default this is just the function
+    name unless the interactive CLI script gets the '-s' option.
+    """
+    if CLI_ARGS.short:
+        first, second = func_name.split('_', 1)
         if second == "card":
             return first[0:2]
         else:
@@ -85,15 +99,19 @@ def name_str(name):
             else:
                 return "{}{}".format(first[0], second[0])
     else:
-        return name
+        return func_name
 
-def help_str(name, func):
-    return "({0}) {1}".format(name, func.__doc__)
+def help_str(func):
+    """
+    Create a help string for an action from the corresponding functions name and
+    doc string.
+    """
+    return "({0}) {1}".format(func.__name__, func.__doc__)
 
 # Add a parser for each card function
 for name, data in CARD_FUNCTIONS.items():
-    sub_parser = subparsers.add_parser(name_str(name), add_help=False,
-            help=help_str(name, data['function']))
+    sub_parser = SUBPARSERS.add_parser(name_str(name), add_help=False,
+                                       help=help_str(data['function']))
     sub_parser.set_defaults(action=data['function'])
     for arg in data['argspec'].args:
         sub_parser.add_argument(arg)
@@ -102,8 +120,8 @@ for name, data in CARD_FUNCTIONS.items():
 
 # Add a parser for each card set function
 for name, data in SET_FUNCTIONS.items():
-    sub_parser = subparsers.add_parser(name_str(name), add_help=False,
-            help=help_str(name, data['function']))
+    sub_parser = SUBPARSERS.add_parser(name_str(name), add_help=False,
+                                       help=help_str(data['function']))
     sub_parser.set_defaults(action=data['function'])
     for arg in data['argspec'].args:
         sub_parser.add_argument(arg)
@@ -113,8 +131,13 @@ for name, data in SET_FUNCTIONS.items():
 ################################################################################
 
 def execute(parsed_args):
+    """
+    Use the interactive CLI arguments to execute the corresponding function
+    """
     args_dict = vars(parsed_args)
     func = args_dict.pop('action')
+    # pylint: disable=bad-builtin
+    # Complaining about using map
     argstr = ','.join(map(
         lambda i: '='.join([i[0], str(i[1])]),
         args_dict.items()))
@@ -123,52 +146,66 @@ def execute(parsed_args):
         ret = func(**args_dict)
         print("\n{0}({1}) returned: {2}".format(func.__name__, argstr, ret))
         return ret
-    except Exception as e:
-        print("\n{0}({1}) encountered an error: {2}\n".format(func.__name__, argstr, e))
+
+    # pylint: disable=broad-except
+    # Intentionally catching all exceptions to prevent interactive CLI from
+    # exiting
+    except Exception as error:
+        print("\n{0}({1}) encountered an error: {2}\n".format(
+            func.__name__, argstr, error))
         input("Press enter for traceback...")
         traceback.print_exc()
         print("")
         input("Press enter to continue...")
 
 def get_help(action=None):
+    """
+    Depending on the action specified, get the help output from the
+    corresponding action parser.
+    """
     try:
         parser = PARSERS[action]
     except KeyError:
         parser = PARSERS['base']
     return parser.format_help()
 
-def quit(*args, **kwargs):
+# pylint: disable=redefined-outer-name, unused-argument
+# The *args, **kwargs format is necessary due to how ArgumentParser objects
+# call action functions, but are not necessary for exiting.
+def exit_shell(*args, **kwargs):
+    """
+    Exit the interactive CLI
+    """
     print("Exiting...")
     exit(0)
 
 # The quit option must be added after the quit function is defined
-sub_parser = subparsers.add_parser('quit', add_help=False, help="Exit the interactive CLI")
-sub_parser.set_defaults(action=quit)
+SUB_PARSER = SUBPARSERS.add_parser('quit', add_help=False, help="Exit the interactive CLI")
+SUB_PARSER.set_defaults(action=exit_shell)
 
 ################################################################################
 
 # Primary loop
-context = None
+CONTEXT = None
 while True:
 
-    prompt = "{line}{0}\n>>> ".format(get_help(context),
-            line = "-"*80 + "\n"
-            )
+    PROMPT = "{line}{0}\n>>> ".format(get_help(CONTEXT),
+                                      line="-"*80 + "\n")
 
-    user_input = input(prompt)
-    context = None
-    if len(user_input) == 0:
+    USER_INPUT = input(PROMPT)
+    CONTEXT = None
+    if len(USER_INPUT) == 0:
         continue
 
     try:
-        args = parser.parse_args(user_input.split())
+        ARGS = PARSER.parse_args(USER_INPUT.split())
 
     # Don't quit on errors, just print a message and loop again
-    except ArgumentParser.TooFewArguments as e:
-        context = user_input.split()[0]
+    except ArgumentParser.TooFewArguments:
+        CONTEXT = USER_INPUT.split()[0]
         continue
-    except ArgumentParser.Error as e:
+    except ArgumentParser.Error:
         continue
 
-    execute(args)
+    execute(ARGS)
 
