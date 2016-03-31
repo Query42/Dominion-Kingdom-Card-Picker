54e182d17feb910b2998be0abdb4af2cd5ad45ac
David Kiesel-Nield <dkiesel-nield@bgcportland.org>
2016-03-30 17:09:24 -0700

    More tests
    Added additional unit tests.

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import json
+import random
 import unittest
 
 import card_functions
@@ -16,13 +18,31 @@ class TestCardFunctions(unittest.TestCase):
         card_functions.input = test_input
         assert card_functions.multiline() == "test1\ntest2"
         
-    def test_create_card_asks_for_set(self):
-        def test_input(*args): return(return(True))
-        card_functions.input = test_input
-        assert card_functions.create_card() == True
+##    def test_create_card_asks_for_set(self):  #Not currently testing as
+##        def test_input(*args): return(True)   #it alters the database
+##        card_functions.input = test_input
+##        assert card_functions.create_card() == True
 
-    def test_examine_card(self):
-        pass
+    def test_select_card(self):
+        card = card_functions.select_card("Cellar")
+        assert card.name == "Cellar"
+
+
+class TestSetFunctions(unittest.TestCase):
+    def test_load_sets_changes_global(self):
+        def load(*args):
+            return True
+        json.load = load
+        card_sets.load_sets()
+        assert card_sets.user_sets
+
+    def test_load_sets_default(self):
+        def load(*args):
+            int('test')
+        json.load = load
+        card_sets.load_sets()
+        assert card_sets.user_sets == card_sets.SETS
+
 
 if __name__ == '__main__':
     unittest.main()
